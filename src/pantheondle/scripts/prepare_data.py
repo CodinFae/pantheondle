5714b55d84349394f8916e0860a5b9c40894bf13
Sam <samuel.gillieron@gmail.com>
2026-09-25 12:24:12 +0200

    fix: reduce size of the data by prefiltering in prepare_data

--- src/pantheondle/scripts/prepare_data.py
+++ src/pantheondle/scripts/prepare_data.py
@@ -4,8 +4,22 @@ from pathlib import Path
 
 import pandas as pd
 
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+columns_to_keep = [
+    "bplace_name",
+    "bplace_lat",
+    "bplace_lon",
+    "birthyear",
+    "dplace_name",
+    "dplace_lat",
+    "dplace_lon",
+    "deathyear",
+    "gender",
+    "occupation",
+    "name",
+    "hpi"]
 
 def prepare_data(input_path: Path, output_dir: Path) -> None:
     df = pd.read_csv(input_path)
@@ -15,6 +29,19 @@ def prepare_data(input_path: Path, output_dir: Path) -> None:
     df = df[pd.notna(df["bplace_name"])]
     df = df[pd.notna(df["bplace_lat"])]
     df = df[pd.notna(df["dplace_lat"])]
+    df = df[pd.notna(df["bplace_lon"])]
+    df = df[pd.notna(df["dplace_lon"])]
+    df = df[pd.notna(df["birthyear"])]
+    df = df[pd.notna(df["deathyear"])]
+
+    # Keep only useful columns
+    df = df[columns_to_keep]
+
+    # Format to correct type
+    df = df.astype({"birthyear":"int16", "deathyear":"int16"})
+
+    logger.debug(df.head())
+    logger.debug(df.dtypes)
     logger.info(f"{len(df)} persons remaining")
 
     output_path = output_dir / "persons.parquet"
